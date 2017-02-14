a80e8e97153b5a33e19f7f109239c1dfa9747ea0
altoros-ci <konstantin.burtsev@gmail.com>
2017-02-14 23:44:40 +0000

    Bump bosh-init to 0.0.100

--- attributes/bosh-init.rb
+++ attributes/bosh-init.rb
@@ -1,2 +1,2 @@
-default['bosh-init']['release'] = 'https://s3.amazonaws.com/bosh-init-artifacts/bosh-init-0.0.99-linux-amd64'
-default['bosh-init']['checksum'] = 'e72831813e1c020e5087d6c05a11dd638e628ad9315442d7d235db4d59d6e5ea'
+default['bosh-init']['release'] = 'https://s3.amazonaws.com/bosh-init-artifacts/bosh-init-0.0.100-linux-amd64'
+default['bosh-init']['checksum'] = 'ae576d907cd1fbec5d8bbcb78bfdedef572e1c6ec6d88041334ae26a416554ed'
