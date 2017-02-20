812efa0b75eba996453f7a583523d7ec7700b835
altoros-ci <konstantin.burtsev@gmail.com>
2017-02-20 14:05:36 +0000

    Bump spruce to 1.8.10

--- attributes/spruce.rb
+++ attributes/spruce.rb
@@ -1,2 +1,2 @@
-default['spruce']['checksum'] = 'a6662c275dd2e0e278bf140f6382a085313101e220980b664ccd101b862b2ab7'
-default['spruce']['release'] = 'https://github.com/geofffranks/spruce/releases/download/v1.8.9/spruce-linux-amd64'
+default['spruce']['checksum'] = '02bf966cfeb77a2a65c99a1169f5096bde09df8298b923edfc31df35dbd825e0'
+default['spruce']['release'] = 'https://github.com/geofffranks/spruce/releases/download/v1.8.10/spruce-linux-amd64'
