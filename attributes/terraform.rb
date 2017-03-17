b42e08666d63114fdab2b671e68bfd443712de13
altoros-ci <konstantin.burtsev@gmail.com>
2017-03-17 22:55:29 +0000

    Bump terraform to 0.9.1

--- attributes/terraform.rb
+++ attributes/terraform.rb
@@ -1,2 +1,2 @@
-default['terraform']['release'] = 'https://releases.hashicorp.com/terraform/0.9.0/terraform_0.9.0_linux_amd64.zip'
-default['terraform']['checksum'] = '0965cf0f9d373a550163aef4732438c201c7271ea5f96a902863248cb7f39f1c'
+default['terraform']['release'] = 'https://releases.hashicorp.com/terraform/0.9.1/terraform_0.9.1_linux_amd64.zip'
+default['terraform']['checksum'] = 'b3b18a719258dcc02b7b972eedf417be0b497e4129063711bca82877dbe65553'
