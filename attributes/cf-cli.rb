bf480d2aafbda5739d543e10d450eb8402849bdd
altoros-ci <konstantin.burtsev@gmail.com>
2017-04-07 00:33:08 +0000

    Bump cf-cli to 6.26.0

--- attributes/cf-cli.rb
+++ attributes/cf-cli.rb
@@ -1,2 +1,2 @@
-default['cf']['checksum'] = '0e9550f8aae1e02b099f052c92686e8d1ecf0a97a48cdccba8c70cb68167e762'
-default['cf']['release'] = 'https://s3-us-west-1.amazonaws.com/cf-cli-releases/releases/v6.25.0/cf-cli_6.25.0_linux_x86-64.tgz'
+default['cf']['checksum'] = '5e9018d965e94ef34047346170c8b487bdcaecacd3860c06cf7adbfa3145fab2'
+default['cf']['release'] = 'https://s3-us-west-1.amazonaws.com/cf-cli-releases/releases/v6.26.0/cf-cli_6.26.0_linux_x86-64.tgz'
