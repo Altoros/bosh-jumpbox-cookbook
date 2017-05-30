e796a06196358be889fd88e0ef68aadd1da2728a
altoros-ci <konstantin.burtsev@gmail.com>
2017-05-30 18:44:19 +0000

    Bump fly to 3.1.0

--- attributes/fly.rb
+++ attributes/fly.rb
@@ -1,2 +1,2 @@
-default['fly']['checksum'] = '0c439d8363e5d81e61f02f3a4cd0d8051ba276c2ed81a27e985416fc230e296d'
-default['fly']['release'] = 'https://github.com/concourse/concourse/releases/download/v3.0.1/fly_linux_amd64'
+default['fly']['checksum'] = '80e75f9c95a2b11e92ffe9f585b62527ed076af5b104ebaccd164e7b7bf1819c'
+default['fly']['release'] = 'https://github.com/concourse/concourse/releases/download/v3.1.0/fly_linux_amd64'
