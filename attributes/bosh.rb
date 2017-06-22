bf22c483180ddc4f4ffb5aa60dfe485007129547
altoros-ci <konstantin.burtsev@gmail.com>
2017-06-22 18:53:45 +0000

    Bump bosh to 2.0.25

--- attributes/bosh.rb
+++ attributes/bosh.rb
@@ -1,2 +1,2 @@
-default['bosh']['release'] = 'https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-2.0.22-linux-amd64'
-default['bosh']['checksum'] = '0c1ec8af796b1f1124ff8f83f8581865fa5c0fe851ac1fe8914cf624c1b0ba79'
+default['bosh']['release'] = 'https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-2.0.25-linux-amd64'
+default['bosh']['checksum'] = '9b1b44d1ac1aa0176174901917c0b0b7cc64ac9bf9b47cee0241381b354f6b72'
